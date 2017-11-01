c254114e5717d276989d7c595a867f4046e5b299
Paul <pfryer@c4b301bb4d09.ant.amazon.com>
2017-11-01 08:09:21 -0700

    first checkin

---
+++ DotStepStarter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DotStepStarter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
