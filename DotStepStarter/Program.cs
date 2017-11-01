5497ad00337357d228ba2998c36e611a1ccba03e
paulfryer@macrowebservices.com <paulfryer@macrowebservices.com>
2017-11-01 08:12:40 -0700

    test

--- DotStepStarter/Program.cs
+++ DotStepStarter/Program.cs
@@ -6,7 +6,7 @@ namespace DotStepStarter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! Update 2.");
         }
     }
 }
