0170d40aeab7168eaabdeb34ff144280031d966e
Paul Fryer <paulfryer@macrowebservices.com>
2017-11-03 07:37:06 -0700

    trying to get codepipeline working.

--- Builder/Program.cs
+++ Builder/Program.cs
@@ -10,14 +10,18 @@ namespace DotStepStarter
     {
         static void Main(string[] args)
         {
+            var releaseDirectory = args.Length > 0 ?
+                args[0] :
+                "bin/release";
+         
             var template = DotStepBuilder.BuildCloudFormationTemplate<SimpleCalculator>();
-            File.WriteAllText("template.json", template);
+            File.WriteAllText($"{releaseDirectory}/template.json", template);
 
-            var s3Url = Environment.GetEnvironmentVariable("CODEBUILD_SOURCE_REPO_URL");
+            var s3Url = Environment.GetEnvironmentVariable("CODEBUILD_SOURCE_REPO_URL") ??
+                "s3://unknown/unknown/build.zip";
             Console.WriteLine($"S3 URL: {s3Url}");
             var s3Bucket = s3Url.Split('/')[2];
             var s3Key = s3Url.Replace(s3Bucket + "/", string.Empty);
-
             var config = JsonConvert.SerializeObject(new
             {
                 Parameters = new
@@ -26,8 +30,8 @@ namespace DotStepStarter
                     S3CodeKey = s3Key
                 }
             });
-
-            File.WriteAllText("config.json", config);            
+            File.WriteAllText($"{releaseDirectory}/config.json", config);
+                    
         }
     }
 }
