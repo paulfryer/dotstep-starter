30f0c33efb6a9525e9502029a575b58bba263ed6
Paul Fryer <paulfryer@macrowebservices.com>
2017-11-03 14:48:42 -0700

    trying to get s3 uploads working.

--- Builder/Program.cs
+++ Builder/Program.cs
@@ -1,5 +1,8 @@
-﻿using DotStep.Builder;
+﻿using Amazon.S3;
+using Amazon.S3.Model;
+using DotStep.Builder;
 using DotStepStarter.StateMachines.Calculator;
+using System;
 using System.IO;
 
 namespace DotStepStarter
@@ -8,9 +11,26 @@ namespace DotStepStarter
     {
         static void Main(string[] args)
         {
+            IAmazonS3 s3 = new AmazonS3Client();
             var releaseDirectory = args.Length > 0 ? args[0] : "bin/release";
             var template = DotStepBuilder.BuildCloudFormationTemplate<SimpleCalculator>();
-            File.WriteAllText($"{releaseDirectory}/template.json", template);
+            var path = $"{releaseDirectory}/template.json";
+            File.WriteAllText(path, template);
+
+            var version = Environment.GetEnvironmentVariable("CODEBUILD_SOURCE_VERSION");
+
+            var bucket = version.Split(':')[5];
+            var key = version.Split('/')[1] + "/template.json";
+
+            s3.PutObjectAsync(new PutObjectRequest
+            {
+                BucketName = bucket,
+                Key = key,
+                FilePath = path,
+                ContentType = "application/json",
+                ServerSideEncryptionMethod = ServerSideEncryptionMethod.AES256
+            }).Wait();
+
         }
     }
 }
