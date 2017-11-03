bf005d62da1487eda856422e991f9a4831ad2997
Paul Fryer <paulfryer@macrowebservices.com>
2017-11-03 09:00:12 -0700

    work

--- Builder/Program.cs
+++ Builder/Program.cs
@@ -1,7 +1,5 @@
 ﻿using DotStep.Builder;
 using DotStepStarter.StateMachines.Calculator;
-using Newtonsoft.Json;
-using System;
 using System.IO;
 
 namespace DotStepStarter
@@ -10,28 +8,9 @@ namespace DotStepStarter
     {
         static void Main(string[] args)
         {
-            var releaseDirectory = args.Length > 0 ?
-                args[0] :
-                "bin/release";
-         
+            var releaseDirectory = args.Length > 0 ? args[0] : "bin/release";
             var template = DotStepBuilder.BuildCloudFormationTemplate<SimpleCalculator>();
             File.WriteAllText($"{releaseDirectory}/template.json", template);
-
-            var s3Url = Environment.GetEnvironmentVariable("CODEBUILD_SOURCE_REPO_URL") ??
-                "s3://unknown/unknown/build.zip";
-            Console.WriteLine($"S3 URL: {s3Url}");
-            var s3Bucket = s3Url.Split('/')[2];
-            var s3Key = s3Url.Replace(s3Bucket + "/", string.Empty);
-            var config = JsonConvert.SerializeObject(new
-            {
-                Parameters = new
-                {
-                    S3CodeBucket = s3Bucket,
-                    S3CodeKey = s3Key
-                }
-            });
-            File.WriteAllText($"{releaseDirectory}/config.json", config);
-                    
         }
     }
 }
